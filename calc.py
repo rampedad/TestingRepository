e582e89e017da6407ca0441551e46896d8ff9723
Ramesh P <rameshpedada@gmail.com>
2019-09-16 09:08:43 +0000

    second commit

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add (x, y):
+    """Add Function"""
+        return x + y
+        pass
+
+    def subtract(x, y):
+        """Subtract Function"""
+            return x - y
+
+        def multiply(x, y):
+            """Myltiply Function"""
+                retury x * y
+
+            def divide(x, y):
+                """Divide Function"""
+                    return x/y
+                pass
